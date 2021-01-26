9bee58fc5ed1e5462459ccd8b3147028ee735564
fabchirag <chiraglimbachia@gmail.com>
2021-01-26 10:53:06 +0000

    midlibs

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+# string concatenation (aka how to put the strings togeather)
+
+# youtuber = "Chirag Limbachia"
+#
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+
+adjective = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+
+madlib = (f"Computer programmer is so {adjective}! It makes me excited all the time" '\n'
+f"I love to {verb1} like stay hydrated and {verb2} like you are a {famous_person}")
+
+print(madlib)
